04b6a3716e73264ca19669ff52560436033fb089
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-08-14 22:38:09 +0300

    code3 edit

--- C3_401js/BinarySearch.js
+++ C3_401js/BinarySearch.js
@@ -1,15 +1,28 @@
 'use strict'
 let binarySearch=(sortedArr,key)=>{
-    let idx ;
-    for (let index = 0; index < sortedArr.length; index++) {
-        if(sortedArr[index]==key) 
-        {  idx = index
-            return idx}}
-    if(typeof idx ==='undefined')
-    {
-        return -1
+    // let idx ;
+    let start = 0;
+    let end = sortedArr.length-1;
+    // for (let index = 0; index < sortedArr.length; index++) {
+    //     if(sortedArr[index]==key) 
+    //     {  idx = index
+    //         return idx}}
+    // if(typeof idx ==='undefined')
+    // {
+    //     return -1
+    // }
+    while (start <= end) {
+        let mid = Math.ceil((start + end) / 2);
+        if (sortedArr[mid] === key) {
+            return mid;
+        } else if (sortedArr[mid] < key) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
+    return -1;
 }
 let array = [1,2,3,4,5];
-let key = 5
+let key = 4
 console.log(`The index of ${key} is ${binarySearch(array,key)}`)
