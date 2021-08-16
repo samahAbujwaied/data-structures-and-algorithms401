34ac12e6bf5ced3e98e7ae764bf4997bb5f29386
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-08-16 17:15:15 +0300

    code7

---
+++ C7_401js/__tests__/ll.test.js
@@ -0,0 +1,49 @@
+'use strict';
+const LinkedList=require('../linked-List');
+describe('test on Linked Lists',()=>{
+  let linkedList=new LinkedList();
+  it(' empty linked list',()=>{
+    expect(linkedList.head).toBeNull();
+  });
+  it(' insert at the linked list',()=>{
+    linkedList.insert(1);
+    expect(linkedList.head.value).toEqual(1);
+  });
+  it('point on the first node',()=>{
+    expect(linkedList.head.value).toEqual(1);
+  });
+  it('adding more than node ',()=>{
+    linkedList.insert(2);
+    linkedList.insert(3);
+    expect(linkedList.head.value).toEqual(3);
+  });
+  it('checking for value if it on inside node or not',()=>{
+    expect(linkedList.includes(3)).toBeTruthy();
+    expect(linkedList.includes(4)).toBeFalsy();
+  });
+  it('return all the linked list',()=>{
+    let newLinkedList=new LinkedList();
+    expect(linkedList.toString()).toEqual('{3} -> {null}');
+    expect(newLinkedList.toString()).toEqual('{null}');
+  });
+  it('add node on the first', ()=> {
+    linkedList.insertBefore(3,4);
+    expect(linkedList.head.value).toEqual(3);
+    expect(linkedList.toString()).toEqual('{3} -> {null}');
+  
+  });
+  it('insert after the middle node', ()=> {
+    linkedList.insertAfter(3, 7);
+    console.log( linkedList)
+    expect(linkedList.toString()).toEqual('{3} -> {7} -> {null}');
+  });
+  it('add node on the end', ()=> {
+    linkedList.insertAfter(1, 0);
+    expect(linkedList.toString()).toEqual('{3} -> {7} -> {null}');
+  });
+  it('add node on the end', ()=> {
+    linkedList.kthFromEnd();
+    expect(linkedList.kthFromEnd()).toBeTruthy();
+  });
+   
+});
---
+++ C7_401js/linked-List.js
@@ -0,0 +1,128 @@
+'use strict'
+const Node=require('./node');
+class linkedList{
+    constructor(){
+        this.head=null;
+    }
+    append(value) {
+        let cur = this.head;
+      console.log('value',value);
+        while (cur) {
+          if (cur.next === null) {
+            console.log(cur.next);
+            cur.next = new Node(value);
+            console.log(cur.next);
+            return;
+          }
+          cur = cur.next;
+        }
+      }  
+
+      insert(value){
+        try {
+          this.head = new Node(value) ;
+          console.log(`insert ${this.head}`);
+        }
+        catch(error) {
+          console.error(`Error while inserting ${error}`);
+        }
+      }
+
+      includes(value) {
+        let cur = this.head;
+        while (cur) {
+          if (value === cur.value) {
+            return true;
+          } else if (cur.next === null) {
+            return false;
+          } else {
+            cur = cur.next;
+          }
+        }
+      }
+    
+      toString() {
+        let cur= this.head;
+        let string = '';
+        while (cur) {
+          let stringTwo = cur.value;
+          cur = cur.next;
+          string += `{${stringTwo}} -> `;
+        }
+        string += '{null}';
+        return string;
+      }
+    
+     
+      
+    insertBefore(value, newValue) {
+    let cur = this.head;
+    let newNode = new Node(newValue);
+    while (cur.next !== null) {
+      if (cur.value === value) {
+        newNode.next = cur;
+        this.head = newNode;
+      } else if (cur.next.value === value) {
+        let temp = cur.next;
+        cur.next = newNode;
+        newNode.next = temp;
+        return;
+      }
+      cur = cur.next;
+    }
+
+  }
+  insertAfter(value, newValue){
+    let node=new Node(newValue);
+    if(this.head){
+      let cur=this.head;
+      while(cur){
+        if(cur.value===value){
+          let temp=cur.next;
+          cur.next=node;
+          node.next=temp;
+          return ;
+        }
+        cur=cur.next;
+      }
+    }
+    return ;
+  }
+
+  kthFromEnd(k = 0) {
+    if(isNaN(k))
+     { throw new Error('Invalid K'); }
+    if(this.size < k || k < 0) 
+    { throw new Error('k is not found'); }
+    if(!this.size) 
+    { throw new Error('not have a nodes in linked list'); }
+    if(this.size === 1 || this.size === k) 
+    { return this.head.value; }
+    let cur = this.head;
+    let res = this.head.value;
+    for(let i = 0; i < (this.size - k); i++) {
+      res = cur.value;
+      cur = cur.next;
+    }
+    return res;
+  }
+
+}
+
+let newlink = new linkedList();
+console.log(`the head of linked list ${newlink}`);
+   
+  newlink.append('noor');
+  newlink.append('qasem')
+  console.log('newlink',newlink.toString());
+    let val1 = 'rawan';
+    let val2 = 'roqaia'  
+    let val3 = 'samah'
+    newlink.insert(val1);
+    newlink.insert(val2);
+    newlink.insert(val3);
+    console.log(`new ${newlink}`);
+    console.log('newlink',newlink.toString());
+    newlink.includes('rawan')
+    newlink.toString();
+module.exports=linkedList;
